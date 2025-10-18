7eda5b28e3e8ebef3dfe3e5788bd39fa27f7c0f4
Ankit Lal Sinha <ankitsinha25061909@gmail.com>
2025-10-18 12:06:49 +0530

    resize the team image size

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -161,7 +161,7 @@ const About = () => {
                 <img
                   src={member.image}
                   alt={member.name}
-                  className="w-full h-80 object-cover"
+                  className="w-full h-100 object-cover"
                 />
                 <div className="p-6">
                   <h3 className="text-xl font-semibold text-gray-900 mb-2">
