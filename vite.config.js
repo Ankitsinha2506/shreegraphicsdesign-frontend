4a907e493db85e941302e010f033a4c2942cc621
Ankit Lal Sinha <ankitsinha25061909@gmail.com>
2026-02-18 14:51:01 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5003',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+    plugins: [react()],
+
+    optimizeDeps: {
+        exclude: ["@imgly/background-removal"],
+    },
+
+    server: {
+        proxy: {
+            "/api": {
+                target: process.env.VITE_API_URL || "http://localhost:5003",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+});
